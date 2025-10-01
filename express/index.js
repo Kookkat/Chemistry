f5d1d7ed641e24f88879dbd5264401c5ab97e3b3
Kookkat <aykeil24@gmail.com>
2025-10-01 06:58:48 -0500

    Still nothing

--- express/index.js
+++ express/index.js
@@ -1,7 +1,22 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.listen(3000,() => {
-    console.log(`Server is running on port 3000!`)
-})
+app.get("{/*path}", async (req, res, next) => {
+  console.log(req.path);
+  next();
+});
+app.get("{/*path}", async (req, res) => {
+  return res.send(
+    `<script>
+    let i = 0;
+    setInterval(function(){
+    history.replaceState(null,'',i.toString())
+    i++;
+    },250)
+    </script>`
+  );
+});
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000!`);
+});
---
+++ gas.js
@@ -0,0 +1,7 @@
+//Simple pv=nrt equation
+//Pressure*Volume=# of Moles*constant*temp
+//usally in atm*L=mol*0.08206L*atm/(mol*k) or 0.08206 l atm mol^-1 K^-1
+//should be easy, just math, but also add conversions
+//ex mmHg--->atm
+const prompt = require("prompt-sync")();
+var compound = prompt("What factor is missing?(ex. V, P, Mol, K)");

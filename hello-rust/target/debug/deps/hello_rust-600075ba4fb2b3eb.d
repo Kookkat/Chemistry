3063cdbdcf59c4060a0687a6372845fa28c810bf
Kookkat <aykeil24@gmail.com>
2025-10-07 20:58:45 -0500

    NVM no rust in this

--- hello-rust/target/debug/deps/hello_rust-600075ba4fb2b3eb.d
+++
@@ -1,5 +0,0 @@
-/workspaces/Chemistry/hello-rust/target/debug/deps/hello_rust-600075ba4fb2b3eb.d: src/main.rs
-
-/workspaces/Chemistry/hello-rust/target/debug/deps/hello_rust-600075ba4fb2b3eb: src/main.rs
-
-src/main.rs:
--- hello-rust/target/debug/deps/unicode_linebreak-181f8a28aa5ba62a.d
+++
@@ -1,9 +0,0 @@
-/workspaces/Chemistry/hello-rust/target/debug/deps/unicode_linebreak-181f8a28aa5ba62a.d: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/shared.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/tables.rs
-
-/workspaces/Chemistry/hello-rust/target/debug/deps/libunicode_linebreak-181f8a28aa5ba62a.rlib: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/shared.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/tables.rs
-
-/workspaces/Chemistry/hello-rust/target/debug/deps/libunicode_linebreak-181f8a28aa5ba62a.rmeta: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/shared.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/tables.rs
-
-/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/lib.rs:
-/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/shared.rs:
-/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-linebreak-0.1.5/src/tables.rs:
